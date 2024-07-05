906f8998ffe00caaad425ef44b51c4354ec144fa
Morgan <103436411+MorganKryze@users.noreply.github.com>
2024-07-06 00:29:32 +0200

    🌟 update scripts

--- scripts/.bash_aliases
+++ scripts/.bash_aliases
@@ -7,6 +7,9 @@ alias ...="cd ../.."
 alias ....="cd ../../.."
 alias .....="cd ../../../.."
 
+unalias ls
+alias ls="ls -latr"
+
 # Navigation
 alias go_rpi="cd ~/Documents/Projects/Embed-Control/src/command/Rpi/"
 alias go_esp="cd ~/Documents/Projects/Embed-Control/src/command/ESP/"
@@ -15,6 +18,7 @@ alias go_home="cd ~/Documents/"
 # Python
 alias py="python"
 alias pip-req="pip list --format=freeze > requirements.txt"
+alias py-api-env="source ~/Documents/Env/api_env/bin/activate"
 
 # Copy utils
 alias copy="| pbcopy"
@@ -26,4 +30,5 @@ alias reload="exec ${SHELL} -l"
 alias path='echo -e ${PATH//:/\\n}'
 
 # Create wifi hotspot
-alias hotspot="sudo nmcli device wifi hotspot ssid ntw_TRAVELERS password TRAVELERS"
+alias hotspot-on="sudo nmcli device wifi hotspot ssid ntw_TRAVELERS password TRAVELERS"
+alias hotspot-off="sudo nmcli device wifi hotspot con-name ntw_TRAVELERS"
--- scripts/setup-bootstrap.sh
+++ scripts/setup-bootstrap.sh
@@ -6,16 +6,16 @@ echo "----- Installing the alias config -----"
 curl -O https://raw.githubusercontent.com/THE-TRAVELERS/Embed-Control/main/scripts/.bash_aliases
 mv .bash_aliases ~/.bash_aliases
 
-echo "----- Finished installing the alias config -----"
+echo "----- Finished installing the alias config -----\n"
 
 
 # Install the rc.local file for running the hotspot on boot
 echo " ----- Installing the hotspot config -----"
 
 curl -O https://raw.githubusercontent.com/THE-TRAVELERS/Embed-Control/main/scripts/rc.local
-mv rc.local /etc/rc.local
+sudo mv rc.local /etc/rc.local
 
-echo "----- Finished installing the hotspot config -----"
+echo "----- Finished installing the hotspot config -----\n"
 
 # Setting up src files
 echo "----- Setting up the src files -----"
@@ -26,32 +26,33 @@ mkdir Env
 cd Projects
 git clone https://github.com/THE-TRAVELERS/Embed-Control.git
 
-echo "----- Finished setting up the src files -----"
+echo "----- Finished setting up the src files -----\n"
 
 # Setting up the virtual environment
 echo "----- Setting up the virtual environment -----"
 
+sudo apt install libcap-dev
 cd ~/Documents/Env
-python3 -m venv api_env
+python3 -m venv -m venv --system-site-packages api_env
 source api_env/bin/activate
 cd ~/Documents/Projects/Embed-Control/src/command/Rpi/
 pip install -r requirements.txt
 
-echo "----- Finished setting up the virtual environment -----"
+echo "----- Finished setting up the virtual environment -----\n"
 
 # Setting up the service
 echo "----- Setting up the service -----"
 
 cd ~
 curl -O https://raw.githubusercontent.com/THE-TRAVELERS/Embed-Control/main/scripts/api.service
-mv api.service /etc/systemd/system/api.service
-systemctl enable api.service
-systemctl start api.service
+sudo mv api.service /etc/systemd/system/api.service
+sudo systemctl enable api.service
+sudo systemctl start api.service
 
-echo "----- Finished setting up the service -----"
+echo "----- Finished setting up the service -----\n"
 
 # Rebooting the system
-echo "----- Rebooting the system in 5s -----"
+echo "----- Rebooting the system in 10s -----"
 
-sleep 5
+sleep 10
 sudo reboot
